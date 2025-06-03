b122d04ed6353af9ae1587c57b52e86c503406a3
momarsow <momar.sow@ltpdampierre.fr>
2025-06-03 09:01:23 +0000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,70 @@
-import logo from './logo.svg';
-import './App.css';
+// src/App.js
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+import React, { Component } from "react";
+import "./App.css";
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      person: {
+        fullName: "Ada Lovelace",
+        bio: "Pionnière de la programmation informatique au 19ème siècle.",
+        imgSrc:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a4/Ada_Lovelace_portrait.jpg/440px-Ada_Lovelace_portrait.jpg",
+        profession: "Mathématicienne & Informaticienne",
+      },
+      show: false,
+      timeElapsed: 0,
+    };
+    this.intervalId = null;
+  }
+
+  toggleShow = () => {
+    this.setState({ show: !this.state.show });
+  };
+
+  componentDidMount() {
+    this.intervalId = setInterval(() => {
+      this.setState((prevState) => ({
+        timeElapsed: prevState.timeElapsed + 1,
+      }));
+    }, 1000);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.intervalId);
+  }
+
+  render() {
+    const { person, show, timeElapsed } = this.state;
+
+    return (
+      <div className="App">
+        <h1>Profil en Classe React</h1>
+        <button onClick={this.toggleShow}>
+          {show ? "Cacher le profil" : "Afficher le profil"}
+        </button>
+
+        {show && (
+          <div className="profile-card">
+            <img src={person.imgSrc} alt={person.fullName} />
+            <h2>{person.fullName}</h2>
+            <p>
+              <strong>Profession :</strong> {person.profession}
+            </p>
+            <p>
+              <em>{person.bio}</em>
+            </p>
+          </div>
+        )}
+
+        <p style={{ marginTop: "1rem" }}>
+          Temps écoulé depuis le montage : {timeElapsed} secondes
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+      </div>
+    );
+  }
 }
 
 export default App;
